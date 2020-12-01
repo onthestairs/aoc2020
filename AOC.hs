cff8be4754d1fcb5a90dc7b01be4f8ad61f66ceb
Austin Platt <austin.platt@searchpilot.com>
2020-12-01 09:47:34 +0000

    solve day 1

--- AOC.hs
+++ AOC.hs
@@ -2,12 +2,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
-module AOC (Solution (..), parse, solve1, solve2, GenericSolution (..)) where
+module AOC (Solution (..), parse, solve1, solve2, GenericSolution (..), Parser, parseFile, parseInt) where
 
 import Control.Lens
 import Control.Lens.TH ()
 import Relude
 import Text.Megaparsec (Parsec, runParser)
+import qualified Text.Megaparsec.Char.Lexer as L
 
 data Solution a b c = Solution
   { _parse :: IO (Maybe a),
@@ -26,6 +27,9 @@ type Parser = Parsec Void Text
 
 parseFile :: Path -> Parser a -> IO (Maybe a)
 parseFile path parse = do
-  fileContents <- readFileText path
+  fileContents <- readFileText ("./data/" <> path)
   let result = runParser parse path fileContents
   pure $ rightToMaybe result
+
+parseInt :: Parser Int
+parseInt = L.decimal
--- Day1.hs
+++ Day1.hs
@@ -3,13 +3,40 @@
 
 module Day1 (solution) where
 
-import AOC
+import AOC (Parser, Solution (..), parseFile, parseInt)
 import Relude
+import Text.Megaparsec (eof, sepBy1)
+import Text.Megaparsec.Char (newline)
 
-solution :: Solution Int Int Text
+type Input = [Int]
+
+parseInts :: Parser Input
+parseInts = sepBy1 parseInt newline <* eof
+
+findPairsWhichSum :: [Int] -> Int -> [(Int, Int)]
+findPairsWhichSum xs target =
+  let pairs = [(x, y) | x <- xs, y <- xs]
+   in filter (\(x, y) -> x + y == target) pairs
+
+solve1 ns = do
+  let pairs = findPairsWhichSum ns 2020
+  (x, y) <- viaNonEmpty head pairs
+  pure $ x * y
+
+findTriplesWhichSum :: [Int] -> Int -> [(Int, Int, Int)]
+findTriplesWhichSum xs target =
+  let pairs = [(x, y, z) | x <- xs, y <- xs, z <- xs]
+   in filter (\(x, y, z) -> x + y + z == target) pairs
+
+solve2 ns = do
+  let triples = findTriplesWhichSum ns 2020
+  (x, y, z) <- viaNonEmpty head triples
+  pure $ x * y * z
+
+-- solution :: Solution Input Input Text
 solution =
   Solution
-    { _parse = pure $ Just 5,
-      _solve1 = id,
-      _solve2 = const "Hello"
+    { _parse = parseFile "1.txt" parseInts,
+      _solve1 = solve1,
+      _solve2 = solve2
     }
