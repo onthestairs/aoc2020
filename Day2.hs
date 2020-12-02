37f4f9d7aa343be5a02f1464a158f7415dcf819d
Austin Platt <austin.platt@searchpilot.com>
2020-12-02 10:11:44 +0000

    solve day 2

--- Day2.hs
+++ Day2.hs
@@ -1,41 +1,69 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Day2 (solution) where
 
 import AOC (Parser, Solution (..), parseFile, parseInt)
+import Control.Lens
 import Relude
 import Text.Megaparsec (eof, sepBy1)
-import Text.Megaparsec.Char (newline)
+import Text.Megaparsec.Char (char, lowerChar, newline, space)
+import Text.Parsec.Combinator (many1)
 
-type Input = [Int]
+data PasswordPolicy = PasswordPolicy
+  { _policyChar :: Char,
+    _policyMin :: Int,
+    _policyMax :: Int
+  }
+  deriving (Show)
 
-parseInts :: Parser Input
-parseInts = sepBy1 parseInt newline <* eof
+makeLenses ''PasswordPolicy
 
-findPairsWhichSum :: [Int] -> Int -> [(Int, Int)]
-findPairsWhichSum xs target =
-  let pairs = [(x, y) | x <- xs, y <- xs]
-   in filter (\(x, y) -> x + y == target) pairs
+type Password = String
 
-solve1 ns = do
-  let pairs = findPairsWhichSum ns 2020
-  (x, y) <- viaNonEmpty head pairs
-  pure $ x * y
+type Input = [(PasswordPolicy, Password)]
 
-findTriplesWhichSum :: [Int] -> Int -> [(Int, Int, Int)]
-findTriplesWhichSum xs target =
-  let pairs = [(x, y, z) | x <- xs, y <- xs, z <- xs]
-   in filter (\(x, y, z) -> x + y + z == target) pairs
+parseInput :: Parser Input
+parseInput = sepBy1 parseLine newline <* eof
 
-solve2 ns = do
-  let triples = findTriplesWhichSum ns 2020
-  (x, y, z) <- viaNonEmpty head triples
-  pure $ x * y * z
+parseLine = do
+  policyMin <- parseInt
+  char '-'
+  policyMax <- parseInt
+  space
+  policyChar <- lowerChar
+  char ':'
+  space
+  password <- some lowerChar
+  pure $ (PasswordPolicy {_policyMin = policyMin, _policyMax = policyMax, _policyChar = policyChar}, password)
+
+solve1 ps = length $ filter (uncurry passwordSatisfiesPolicy) ps
+
+passwordSatisfiesPolicy :: PasswordPolicy -> Password -> Bool
+passwordSatisfiesPolicy passwordPolicy password =
+  let policyChars = length $ filter (\c -> c == (view policyChar passwordPolicy)) password
+   in policyChars >= (view policyMin passwordPolicy) && policyChars <= (view policyMax passwordPolicy)
+
+solve2 ps = length $ filter (uncurry passwordSatisfiesPolicy2) ps
+
+passwordSatisfiesPolicy2 :: PasswordPolicy -> Password -> Bool
+passwordSatisfiesPolicy2 passwordPolicy password =
+  let index1 = view policyMin passwordPolicy
+      index2 = view policyMax passwordPolicy
+      c = view policyChar passwordPolicy
+      charAtIndex1 = password !!? (index1 - 1)
+      charAtIndex2 = password !!? (index2 - 1)
+      index1Correct = charAtIndex1 == Just c
+      index2Correct = charAtIndex2 == Just c
+   in case (index1Correct, index2Correct) of
+        (True, False) -> True
+        (False, True) -> True
+        _ -> False
 
 solution =
   Solution
-    { _parse = parseFile "1.txt" parseInts,
+    { _parse = parseFile "2.txt" parseInput,
       _solve1 = solve1,
       _solve2 = solve2
     }
