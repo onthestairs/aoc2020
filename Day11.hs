846ef66aa87e78f288dbbc0bd3e4ea0c78503c13
Austin Platt <austin.platt@searchpilot.com>
2020-12-11 18:37:38 +0000

    change Map -> Vector

--- Day11.hs
+++ Day11.hs
@@ -1,16 +1,14 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Day11 (solution) where
 
-import AOC (Parser, Solution (..), parseFile, parseInt)
-import qualified Data.Map.Strict as Map
-import qualified Data.MultiSet as Set
-import qualified Data.Sequence as Seq
+import AOC (Parser, Solution (..), parseFile)
+import qualified Data.Vector as Vec
 import Relude hiding (Op)
-import Relude.Extra (maximum1)
 import Text.Megaparsec (eof, sepBy1)
 import Text.Megaparsec.Char (char, newline)
 
@@ -21,23 +19,29 @@ type Input = [[Status]]
 parseInput :: Parser Input
 parseInput = sepBy1 (many $ (char '.' $> F) <|> (char 'L' $> E)) newline <* eof
 
-makeMap cells = Map.fromList [((row, col), value) | (row, rowCells) <- zip [0 ..] cells, (col, value) <- zip [0 ..] rowCells]
+makeMap cells = Vec.fromList $ map Vec.fromList cells
 
 rule E neigbours = if O `notElem` neigbours then O else E
 rule O neigbours = if length (filter (O ==) neigbours) >= 4 then E else O
 rule s _ = s
 
-getNeighbours (row, col) m = [Map.findWithDefault OffGrid coords' m | coords' <- allNeighbours]
+flatten :: Vec.Vector (Vec.Vector a) -> Vec.Vector a
+flatten xss = Vec.concat $ Vec.toList xss
+
+getNeighbours (row, col) m = [getCellValue coords' m | coords' <- allNeighbours]
   where
     allNeighbours = [(row + i, col + j) | i <- [-1, 0, 1], j <- [-1, 0, 1], (i, j) /= (0, 0)]
 
-step m = Map.mapWithKey (\coords v -> rule v (getNeighbours coords m)) m
+getCellValue :: (Int, Int) -> Vec.Vector (Vec.Vector Status) -> Status
+getCellValue (rowIndex, colIndex) grid =
+  fromMaybe OffGrid $
+    do
+      row <- (Vec.!?) grid rowIndex
+      (Vec.!?) row colIndex
 
-showGrid (height, width) m = unlines $ map toText $ [[showChar (Map.findWithDefault OffGrid (row, col) m) | col <- [0 .. width -1]] | row <- [0 .. height -1]]
-  where
-    showChar E = 'L'
-    showChar O = '#'
-    showChar F = '.'
+makeNew f m = Vec.imap (\rowIndex row -> Vec.imap (\colIndex v -> f (rowIndex, colIndex) v) row) m
+
+step m = makeNew (\coords v -> rule v (getNeighbours coords m)) m
 
 iterateUntil p f x = if p x then x else iterateUntil p f (f x)
 
@@ -50,25 +54,23 @@ converge f x = do
 solve1 cells = do
   let m = makeMap cells
   finalState <- converge step m
-  pure $ length $ filter (== O) (Map.elems finalState)
+  pure $ length $ filter (== O) (toList $ flatten finalState)
 
-getVisible :: (Int, Int) -> Map.Map (Int, Int) Status -> [Status]
 getVisible (row, col) m = map findFirstVisible deltas
   where
     deltas = [(i, j) | i <- [-1, 0, 1], j <- [-1, 0, 1], (i, j) /= (0, 0)]
-    getCell coords = Map.findWithDefault OffGrid coords m
-    findFirstVisible (dRow, dCol) = getCell $ iterateUntil (\coords' -> (getCell coords') /= F) (\(row', col') -> (row' + dRow, col' + dCol)) (row + dRow, col + dCol)
+    findFirstVisible (dRow, dCol) = (\coords -> getCellValue coords m) $ iterateUntil (\coords' -> (getCellValue coords' m) /= F) (\(row', col') -> (row' + dRow, col' + dCol)) (row + dRow, col + dCol)
 
 rule2 E visible = if O `notElem` visible then O else E
 rule2 O visible = if length (filter (O ==) visible) >= 5 then E else O
 rule2 s _ = s
 
-step2 m = Map.mapWithKey (\coords v -> rule2 v (getVisible coords m)) m
+step2 m = makeNew (\coords v -> rule2 v (getVisible coords m)) m
 
 solve2 cells = do
   let m = makeMap cells
   finalState <- converge step2 m
-  pure $ length $ filter (== O) (Map.elems finalState)
+  pure $ length $ filter (== O) (toList $ flatten finalState)
 
 solution =
   Solution
